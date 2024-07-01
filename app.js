4fe569b2746502715fbd4d9d4943f38abfe4a3ef
joaonunez <joaovaldiglesias@gmail.com>
2024-07-01 19:25:55 -0400

    ejercicio terminado

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromEuroToYen = function(valueInEuro){
+    let valueInYen = valueInEuro * 156.5;
+    return valueInYen;
+}
+const fromEuroToPound = function(valueInEuro){
+    let valueInPound = valueInEuro * 0.87
+    return valueInPound;
+}
+
 // Esta es mi función que suma dos números
 const sum = (a,b) => {
     return a + b
@@ -8,4 +25,4 @@ console.log(sum(7,3))
 
 // Exporta la función para usarla en otros archivos 
 // (similar a la palabra clave "export" cuando se usa webpack)
-module.exports = { sum };
+module.exports = { sum, fromEuroToDollar, fromEuroToYen, fromEuroToPound };
--- test.js
+++ test.js
@@ -9,3 +9,34 @@ test('adds 14 + 9 to equal 23', () => {
     // Esperamos que la suma de esos 2 números sea 23
     expect(total).toBe(23);
 });
+
+
+test("One euro should be 1.07 dollars", function() {
+    // Importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const dollars = fromEuroToDollar(3.5);
+
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    // Hago mi comparación (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+});
+
+test("One euro should be 156.5 yenes", function(){
+    const { fromEuroToYen } = require('./app.js');
+    const yenes = fromEuroToYen(5);
+    const expected = 5 * 156.5;
+
+    expect(fromEuroToYen(5)).toBe(782.5);
+});
+    
+
+test("One Euro should be 0.87", function(){
+    const{fromEuroToPound} = require ('./app.js');
+    const pounds = fromEuroToPound(10);
+    const expected = 10 * 0.87;
+    expect (fromEuroToPound(10)).toBe(8.7);
+})
